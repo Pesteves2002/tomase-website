33203e704a2456e0291e8586940b2478a480d491
Tomás Esteves <tomasesteves2002@gmail.com>
2025-11-10 16:59:30 +0000

    feat: add new component

--- src/app.rs
+++ src/app.rs
@@ -1,9 +1,11 @@
 use leptos::prelude::*;
-use leptos_meta::{MetaTags, Stylesheet, Title, provide_meta_context};
+use leptos_meta::{provide_meta_context, MetaTags, Stylesheet, Title};
 use leptos_router::{
-    StaticSegment,
     components::{Route, Router, Routes},
+    StaticSegment,
 };
+
+use std::marker::PhantomData;
 
 pub fn shell(options: LeptosOptions) -> impl IntoView {
     view! {
@@ -34,12 +36,12 @@ pub fn App() -> impl IntoView {
         <Stylesheet id="leptos" href="/pkg/tomase-website.css" />
 
         // sets the document title
-        <Title text="Welcome to Leptos" />
+        <Title text="Home | Tomás Esteves" />
 
         // content for this welcome page
         <Router>
             <main>
-                <Routes fallback=|| "Page not found.".into_view()>
+                <Routes fallback=|| "Page not found :(".into_view()>
                     <Route path=StaticSegment("") view=HomePage />
                 </Routes>
             </main>
@@ -47,15 +49,65 @@ pub fn App() -> impl IntoView {
     }
 }
 
+#[component]
+fn SizeOf<T: Sized>(#[prop(optional)] _ty: PhantomData<T>) -> impl IntoView {
+    std::mem::size_of::<T>()
+}
+
+/// Shows progress toward a goal.
+#[component]
+fn ProgressBar<F>(
+    /// How much progress should be displayed.
+    progress: F,
+    /// The maximum value of the progress bar.
+    #[prop(default = 100)]
+    max: u16,
+) -> impl IntoView
+where
+    F: Fn() -> i32 + Send + Sync + 'static,
+{
+    view! {
+        <progress max=max value=progress />
+        <br />
+    }
+}
+
 /// Renders the home page of your application.
 #[component]
 fn HomePage() -> impl IntoView {
     // Creates a reactive value to update the button
-    let count = RwSignal::new(0);
+    let count = RwSignal::new(10);
     let on_click = move |_| *count.write() += 1;
+    let single_count = move || count.get();
+    let double_count = move || count.get() * 2;
+
+    let spread_onto_component =
+        view! { <{..} aria-label="a component with attribute spreading" /> };
 
     view! {
-        <h1>"Welcome to Leptos!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
+        <h1 {..spread_onto_component}>"Tomás Esteves Website"</h1>
+        <button
+            on:click=on_click
+            // class:red=move || count.get() % 2 == 1
+            // set the `style` attribute
+            style="position: absolute"
+            // and toggle individual CSS properties with `style:`
+            style:left=move || format!("{}px", count.get() + 100)
+            style:background-color=move || format!("rgb({}, {}, 100)", count.get(), 100)
+            style:max-width="400px"
+            // Set a CSS variable for stylesheet use
+            style=("--columns", move || count.get().to_string())
+        >
+            "Click Me: "
+            {count}
+        </button>
+        <p>"Double count: " {double_count}</p>
+        <p>"Non reactive: " {count.get_untracked() * 2}</p>
+
+        <ProgressBar progress=single_count />
+        <ProgressBar max=200 progress=double_count />
+
+        <SizeOf<usize> />
+        <SizeOf<String> />
     }
 }
