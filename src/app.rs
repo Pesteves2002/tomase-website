4822b5f1a16bf4f268c626e155ee969fc919737a
Tomás Esteves <tomasesteves2002@gmail.com>
2025-11-10 22:46:20 +0000

    feat: add async and stuff

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,13 @@
 use leptos::prelude::*;
-use leptos_meta::{provide_meta_context, MetaTags, Stylesheet, Title};
+use leptos_meta::{MetaTags, Stylesheet, Title, provide_meta_context};
 use leptos_router::{
-    components::{Route, Router, Routes},
     StaticSegment,
+    components::{Route, Router, Routes},
 };
 
 use std::marker::PhantomData;
+
+use gloo_timers::future::TimeoutFuture;
 
 pub fn shell(options: LeptosOptions) -> impl IntoView {
     view! {
@@ -72,6 +74,58 @@ where
     }
 }
 
+#[component]
+fn NumericInput() -> impl IntoView {
+    let (value, set_value) = signal(Ok(0));
+
+    view! {
+        <h1>"Error Handling"</h1>
+        <label>
+            "Type a number (or something that's not a number!)"
+            <input
+                type="number"
+                on:input:target=move |ev| { set_value.set(ev.target().value().parse::<i32>()) }
+            /> // If an `Err(_) had been rendered inside the <ErrorBoundary/>,
+            // the fallback will be displayed. Otherwise, the children of the
+            // <ErrorBoundary/> will be displayed.
+            // the fallback receives a signal containing current errors
+            <ErrorBoundary fallback=|errors| {
+                view! {
+                    <div class="error">
+                        <p>"Not a number! Errors: "</p>
+                        // we can render a list of errors
+                        // as strings, if we'd like
+                        <ul>
+                            {move || {
+                                errors
+                                    .get()
+                                    .into_iter()
+                                    .map(|(_, e)| view! { <li>{e.to_string()}</li> })
+                                    .collect::<Vec<_>>()
+                            }}
+                        </ul>
+                    </div>
+                }
+            }>
+                <p>
+                    // because `value` is `Result<i32, _>`,
+                    // it will render the `i32` if it is `Ok`,
+                    "You entered " // and render nothing and trigger the error boundary
+                    // if it is `Err`. It's a signal, so this will dynamically
+                    // update when `value` changes
+                    <strong>{value}</strong>
+                </p>
+            </ErrorBoundary>
+        </label>
+    }
+}
+
+async fn load_data(value: i32) -> i32 {
+    // fake a one-second delay
+    TimeoutFuture::new(1000).await;
+    value * 10
+}
+
 /// Renders the home page of your application.
 #[component]
 fn HomePage() -> impl IntoView {
@@ -80,9 +134,20 @@ fn HomePage() -> impl IntoView {
     let on_click = move |_| *count.write() += 1;
     let single_count = move || count.get();
     let double_count = move || count.get() * 2;
+    let is_odd = move || count.get() % 2 == 1;
+    let async_data = LocalResource::new(move || load_data(count.get()));
+    let stable = LocalResource::new(|| load_data(1));
 
     let spread_onto_component =
         view! { <{..} aria-label="a component with attribute spreading" /> };
+
+    let async_result = move || {
+        async_data
+            .get()
+            .map(|value| format!("Server returned {value:?}"))
+            // This loading state will only show before the first load
+            .unwrap_or_else(|| "Loading...".into())
+    };
 
     view! {
         <h1 {..spread_onto_component}>"Tomás Esteves Website"</h1>
@@ -109,5 +174,23 @@ fn HomePage() -> impl IntoView {
 
         <SizeOf<usize> />
         <SizeOf<String> />
+
+        <Show when=move || { is_odd() }>
+            <p>Odd</p>
+        </Show>
+
+        <NumericInput />
+
+        <p>
+            <code>"stable"</code>
+            ": "
+            {move || stable.get()}
+        </p>
+        <p>
+            <code>"async_value"</code>
+            ": "
+            {async_result}
+            <br />
+        </p>
     }
 }
